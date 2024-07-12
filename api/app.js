be36cfa675633474091becde336e2a625dfa131f
vr-1929 <vedraval1905@gmail.com>
2024-07-12 15:41:09 +0530

    testcommit

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app =  express();
 
-app.use("/api/test" , )
+// app.use("/api/test" , )
 
 app.listen(5500 , () => {
     console.log("Server is running on port 5500")
