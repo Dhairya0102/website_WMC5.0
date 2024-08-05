5a1200caec0816ee5eee30def12728648be33471
Dhairya0102 <128136039+Dhairya0102@users.noreply.github.com>
2024-08-05 17:11:40 +0530

    hwy

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://wmc5dhairyavedwebsite1234.vercel.app/",
+  baseURL: "https://wmc5dhairyavedwebsite1234.vercel.app/api",
   withCredentials: true,
 });
 
