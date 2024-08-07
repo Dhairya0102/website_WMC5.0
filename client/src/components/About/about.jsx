8615584ba3cf707be6b0c92c47ec8bc66ce1e2ee
vr-1929 <vedraval1905@gmail.com>
2024-08-07 11:18:09 +0530

    AboutPage

---
+++ client/src/components/About/about.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./about.scss";
+
+function About() {
+  return (
+    <div className="aboutPage">
+      <div className="aboutContent">
+        <h1>About Us</h1>
+        <p>
+          Welcome to GTA Estate, your trusted partner in finding the perfect home.
+          We are committed to providing the best real estate services with a focus on
+          quality, customer satisfaction, and reliability.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
